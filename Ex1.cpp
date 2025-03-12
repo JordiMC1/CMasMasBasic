9b1c2fad32522060a73ce538b48e6e03e7c48ff2
JordiMC1 <jmelia20941@iesjoanramis.org>
2025-03-12 09:49:44 +0100

    a

---
+++ Ex1.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main () {
+    string nom, cognom;
+
+    cout << "Quin es el teu nom? ";
+    cin >> nom;
+    
+    cout << "Quin es el teu cognom? ";
+    cin >> cognom;
+    
+    cout << "Hola, " << nom  << " " << cognom << "!" << endl;
+    
+    return 0;
+}
