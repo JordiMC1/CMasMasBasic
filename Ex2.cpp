11d99fdaf5fc1ee889ee328d22caa38a62742878
JordiMC1 <jmelia20941@iesjoanramis.org>
2025-03-12 12:31:43 +0100

    a

---
+++ Ex2.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main () {
+    int edat;
+
+    cout << "Quin es la teva edat? ";
+    cin >> edat;
+
+    if (edat < 18) {
+        cout << "Tu ets menor d'edat";
+    }
+}
